104198cb1146cb22c588ae097a4110532a4fdaf4
Seishiro Fukuda <108728183+seishiro0410@users.noreply.github.com>
2022-09-16 12:08:21 +0000

    [add]bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -11,5 +11,7 @@ devise_for :customers,skip: [:passwords], controllers: {
 devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
   sessions: "admin/sessions"
 }
+
+get 'admin/homes/top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
