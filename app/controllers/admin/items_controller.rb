b7640833cbe891e16ff3e5dd3bd7f3fb0fc548d5
Seishiro Fukuda <108728183+seishiro0410@users.noreply.github.com>
2022-09-20 15:13:47 +0000

    [add]admin/item show and edit view

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -17,9 +17,17 @@ class Admin::ItemsController < ApplicationController
 
 
   def show
+    @item = Item.find(params[:id])
   end
 
   def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    @item.update(item_params)
+    redirect_to admin_items_path
   end
 
    private
